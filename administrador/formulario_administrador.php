1411956bcfdf5511447ef34ca58586c553ecec3c
jhondomi90 <jhondomi90@gmail.com>
2017-10-05 14:01:26 -0500

    este es un formulario creado desde el equipo utilizando netbeans

---
+++ administrador/formulario_administrador.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+este es un formulario creado desde el equipo utilizando netbeans 
